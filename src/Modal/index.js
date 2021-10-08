20035006657c0f184a4023cb56e2e5c9213aaa70
get <chamoloss@live.fr>
2021-10-08 12:17:11 +0200

    second test design

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -1,5 +1,42 @@
 import React from "react";
 
+import './modal.css';
+import {Button} from '@ornikar/kitt';
+
 export default function Modal() {
-  return <div>TODO</div>;
+  return (
+    <div class='modal-screen'>
+        <div class='modal-main'>
+          
+          <div class='modal-header'>
+            <div><p>Titre</p></div>
+            <div><Button icon='cross' /></div>
+          </div>
+         
+          <div class='modal-content'>
+           <p>Hello !</p>
+           <p>haaaa</p>
+           <p>haaaa</p>
+           <p>haaaa</p>
+           <p>haaaa</p>
+           <p>haaaa</p>
+           <p>haaaa</p>
+           <p>haaaa</p>
+           <p>haaaa</p>
+           <p>haaaa</p>
+          </div>
+          
+          <div class='modal-footer'>
+             <div><p>Label</p></div>
+            <div><Button>Label</Button></div>
+          </div>
+          
+    
+
+
+        </div>
+
+    
+    </div>
+  );
 }
