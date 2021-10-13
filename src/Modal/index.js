e3ddce854112f61ddb4db8ac1ce8157a7a1e4c4e
Valentin Jarysta <valentin.jarysta@critizr.com>
2021-10-13 15:21:03 +0200

    First commit for Ornikar

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -1,5 +1,42 @@
 import React from "react";
+import {Button} from "@ornikar/kitt";
+import "../index.css";
 
 export default function Modal() {
-  return <div>TODO</div>;
+  return (
+    <div className="modal">
+      <div className="modal-header">
+        <h1>Titre</h1>
+        <Button icon="cross" />
+      </div>
+      <div className="modal-content">
+        <p>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+        <p>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+        <p>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+        <p>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+        <p>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+        <p>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+        <p>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+        <p>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+        <p>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+        <p>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+        <p>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+        <p>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+        <p>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+        <p>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+        <p>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+        <p>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+        <p>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+        <p>aaaa</p>
+      </div>
+      <div className="modal-footer">
+        <div className="modal-button">
+          <Button type="primary" stretch>Label</Button>
+        </div>
+        <div className="modal-button">
+          <Button stretch>Label</Button>
+        </div>
+      </div>
+    </div>
+  );
 }
